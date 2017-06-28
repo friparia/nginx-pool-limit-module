0515f5ce575521d6c1db63f95bb34f5b0d0dba9e
friparia <friparia@gmail.com>
2017-06-28 23:13:24 +0800

    fix multiple process segment fault

--- ngx_http_service_pool_limit_module.c
+++ ngx_http_service_pool_limit_module.c
@@ -1,10 +1,13 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include <time.h>
 
 typedef struct{
   u_char color;
   u_char len;
+  ngx_msec_t timer_key;
+  ngx_uint_t ref_cnt;
   u_char *data;
 } ngx_http_service_pool_limit_node_t;
 
@@ -21,7 +24,7 @@ typedef struct{
 
 typedef struct {
   ngx_rbtree_t  *rbtree;
-  ngx_uint_t conn;
+  ngx_uint_t *conn;
 } ngx_http_service_pool_limit_ctx_t;
 
 static char* ngx_http_service_pool_limit(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
@@ -73,6 +76,31 @@ ngx_module_t ngx_http_service_pool_limit_module = {
   NULL,
   NGX_MODULE_V1_PADDING
 };
+static ngx_rbtree_node_t *
+ngx_http_service_pool_limit_timer_rbtree_lookup(ngx_rbtree_t *rbtree, uint32_t hash, ngx_log_t *log){
+  ngx_rbtree_node_t           *node, *sentinel;
+
+  node = rbtree->root;
+  sentinel = rbtree->sentinel;
+
+  while (node != sentinel) {
+
+    if (hash < (uint32_t)node->key) {
+      node = node->left;
+      continue;
+    }
+
+    if (hash > (uint32_t)node->key) {
+      node = node->right;
+      continue;
+    }
+
+    /* hash == node->key */
+    return node;
+  }
+
+  return NULL;
+}
 
 static ngx_rbtree_node_t *
 ngx_http_service_pool_limit_lookup(ngx_rbtree_t *rbtree, uint32_t hash, u_char *key, u_char len)
@@ -158,16 +186,28 @@ static void ngx_service_pool_limit_event_expire_handler(ngx_event_t *ev){
   ngx_slab_pool_t *shpool;
   ngx_http_service_pool_limit_ctx_t *ctx;
   ngx_shm_zone_t *shm_zone;
+  ngx_http_service_pool_limit_node_t *spl;
   ngx_rbtree_node_t *node;
   data = ev->data;
   shm_zone = data->shm_zone;
   node = data->node;
   ctx = shm_zone->data;
+  spl = (ngx_http_service_pool_limit_node_t *) &node->color;
   shpool = (ngx_slab_pool_t *) shm_zone->shm.addr;
   ngx_shmtx_lock(&shpool->mutex);
-  ctx->conn--;
-  ngx_rbtree_delete(ctx->rbtree, node);
-  ngx_slab_free_locked(shpool, node);
+  printf("clean called! %u %d %d\n", (unsigned int)spl->ref_cnt, (unsigned int)time(0), ngx_getpid());
+  if(spl->ref_cnt == 1){
+    printf("deleted!\n");
+    (*ctx->conn)--;
+    ngx_rbtree_delete(ctx->rbtree, node);
+    ngx_slab_free_locked(shpool, node);
+    ngx_slab_free_locked(shpool, data);
+    ngx_slab_free_locked(shpool, ev->log);
+    ngx_slab_free_locked(shpool, ev);
+  }else{
+    printf("no deleted!\n, %d", ngx_getpid());
+    spl->ref_cnt--;
+  }
   ngx_shmtx_unlock(&shpool->mutex);
 }
 
@@ -183,12 +223,16 @@ static ngx_int_t ngx_http_service_pool_limit_handler(ngx_http_request_t* r){
   struct sockaddr_in6 *sin6;
   ngx_slab_pool_t *shpool;
   ngx_rbtree_node_t *node;
+  ngx_rbtree_node_t *timer_node;
   uint32_t hash;
   size_t n;
   ngx_event_t *stop_service_event;
+  ngx_event_t *ev;
   ngx_log_t *event_log;
   ngx_http_service_pool_limit_event_data_t *event_data;
   ngx_http_service_pool_limit_node_t * spl;
+  ngx_msec_t      timer_key;
+  ngx_msec_int_t  timer_diff;
 
   splcf = ngx_http_get_module_srv_conf(r, ngx_http_service_pool_limit_module);
   limit = splcf->limit;
@@ -200,7 +244,7 @@ static ngx_int_t ngx_http_service_pool_limit_handler(ngx_http_request_t* r){
 
   ctx = splcf->shm_zone->data;
 
-  ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: conn-%d, limit-%d, timeout-%d", ctx->conn, limit, timeout);
+  ngx_log_error(NGX_LOG_EMERG, r->connection->log, 0, "SERVICE POOL: pid-%d, conn-%d, limit-%d, timeout-%d", ngx_getpid() ,*ctx->conn, limit, timeout);
 
   switch (r->connection->sockaddr->sa_family){
   case AF_INET:
@@ -219,23 +263,21 @@ static ngx_int_t ngx_http_service_pool_limit_handler(ngx_http_request_t* r){
 
   shpool = (ngx_slab_pool_t *) splcf->shm_zone->shm.addr;
 
-
   ngx_shmtx_lock(&shpool->mutex);
 
   node = ngx_http_service_pool_limit_lookup(ctx->rbtree, hash, key, len);
 
-
-
   if(node == NULL){
-    if(ctx->conn < limit){
+    if(*ctx->conn < limit){
       n = offsetof(ngx_rbtree_node_t, color)
         + offsetof(ngx_http_service_pool_limit_node_t, data)
+        + offsetof(ngx_http_service_pool_limit_node_t, ref_cnt)
+        + offsetof(ngx_http_service_pool_limit_node_t, timer_key)
         + len;
 
       node = ngx_slab_alloc_locked(shpool, n);
       if (node == NULL){
         ngx_shmtx_unlock(&shpool->mutex);
-        ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: Cannot alloc node from shpool");
         return 503;
       }
 
@@ -253,41 +295,104 @@ static ngx_int_t ngx_http_service_pool_limit_handler(ngx_http_request_t* r){
         break;
       }
 
-      ctx->conn++;
-      ngx_rbtree_insert(ctx->rbtree, node);
-      ngx_shmtx_unlock(&shpool->mutex);
-
+      (*ctx->conn)++;
       event_data = ngx_slab_alloc_locked(shpool, sizeof(ngx_http_service_pool_limit_event_data_t));
+
+      if (event_data == NULL){
+        ngx_shmtx_unlock(&shpool->mutex);
+        return 503;
+      }
+
       event_log = ngx_slab_alloc_locked(shpool, sizeof(ngx_log_t));
+
+      if (event_log == NULL){
+        ngx_shmtx_unlock(&shpool->mutex);
+        return 503;
+      }
+
       stop_service_event = ngx_slab_alloc_locked(shpool, sizeof(ngx_event_t));
+      if (stop_service_event == NULL){
+        ngx_shmtx_unlock(&shpool->mutex);
+        return 503;
+      }
 
       event_data->node = node;
       event_data->shm_zone = splcf->shm_zone;
+
+      timer_key = ngx_current_msec + 1000 * timeout;
+      stop_service_event->timer.key = timer_key;
       stop_service_event->data = event_data;
-      stop_service_event->timer_set = 0;
       stop_service_event->log = event_log;
       stop_service_event->handler = ngx_service_pool_limit_event_expire_handler;
-      ngx_event_add_timer(stop_service_event, timeout * 1000);
+
+      spl->ref_cnt = 1;
+      spl->timer_key = timer_key;
+      ngx_rbtree_insert(ctx->rbtree, node);
+
+      printf("insert %p \n", &stop_service_event->timer);
+      ngx_rbtree_insert(&ngx_event_timer_rbtree, &stop_service_event->timer);
+      /* ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: insert key-%d pid-%d event-%d", timer_key, ngx_getpid(), stop_service_event); */
+
+      ngx_shmtx_unlock(&shpool->mutex);
       return NGX_DECLINED;
     }
-    ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: blocked! limit-%d, conn-%d", limit, ctx->conn);
+
+    /* ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: blocked! limit-%d, conn-%d", limit, *ctx->conn); */
     ngx_shmtx_unlock(&shpool->mutex);
     return 503;
   }
 
-  ngx_shmtx_unlock(&shpool->mutex);
-  event_data = ngx_slab_alloc_locked(shpool, sizeof(ngx_http_service_pool_limit_event_data_t));
-  event_log = ngx_slab_alloc_locked(shpool, sizeof(ngx_log_t));
-  stop_service_event = ngx_slab_alloc_locked(shpool, sizeof(ngx_event_t));
 
-  event_data->node = node;
-  event_data->shm_zone = splcf->shm_zone;
-  ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: renew-timer!");
-  stop_service_event->data = event_data;
-  stop_service_event->timer_set = 0;
-  stop_service_event->log = event_log;
-  stop_service_event->handler = ngx_service_pool_limit_event_expire_handler;
-  ngx_event_add_timer(stop_service_event, timeout * 1000);
+  spl = (ngx_http_service_pool_limit_node_t *) &node->color;
+  timer_key = ngx_current_msec + 1000 * timeout;
+  timer_diff = (ngx_msec_int_t) (timer_key - spl->timer_key);
+  if (ngx_abs(timer_diff) >= 500) {
+    ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: search key-%d pid-%d root-%d sentinel-%d", spl->timer_key, ngx_getpid(), ngx_event_timer_rbtree.root->key, ngx_event_timer_rbtree.sentinel->key);
+    timer_node = ngx_http_service_pool_limit_timer_rbtree_lookup(&ngx_event_timer_rbtree, spl->timer_key, r->connection->log);
+    if(timer_node != NULL){
+      ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: delete key-%d pid-%d", spl->timer_key, ngx_getpid());
+      ngx_rbtree_delete(&ngx_event_timer_rbtree, timer_node);
+      ev = (ngx_event_t *)((char*) timer_node - offsetof(ngx_event_t, timer));
+      ngx_slab_free_locked(shpool, ev->data);
+      ngx_slab_free_locked(shpool, ev->log);
+      ngx_slab_free_locked(shpool, ev);
+    }else{
+      ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: not found key-%d set:%d pid-%d", spl->timer_key, spl->ref_cnt, ngx_getpid());
+      spl->ref_cnt++;
+      ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: add key-%d set:%d pid-%d", spl->timer_key, spl->ref_cnt, ngx_getpid());
+    }
+    event_data = ngx_slab_alloc_locked(shpool, sizeof(ngx_http_service_pool_limit_event_data_t));
+    if (event_data == NULL){
+      ngx_shmtx_unlock(&shpool->mutex);
+      return 503;
+    }
+    event_log = ngx_slab_alloc_locked(shpool, sizeof(ngx_log_t));
+    if (event_log == NULL){
+      ngx_shmtx_unlock(&shpool->mutex);
+      return 503;
+    }
+    stop_service_event = ngx_slab_alloc_locked(shpool, sizeof(ngx_event_t));
+    if (stop_service_event == NULL){
+      ngx_shmtx_unlock(&shpool->mutex);
+      return 503;
+    }
+
+
+    event_data->node = node;
+    event_data->shm_zone = splcf->shm_zone;
+
+    stop_service_event->timer.key = timer_key;
+    stop_service_event->data = event_data;
+    stop_service_event->log = event_log;
+    stop_service_event->handler = ngx_service_pool_limit_event_expire_handler;
+    spl->timer_key = timer_key;
+    ngx_rbtree_insert(&ngx_event_timer_rbtree, &stop_service_event->timer);
+
+    /* ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: insert new key-%d pid-%d", timer_key, ngx_getpid()); */
+  }
+  ngx_shmtx_unlock(&shpool->mutex);
+  /* ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "SERVICE POOL: renewing.... set:%d", spl->ref_cnt); */
+
   return NGX_DECLINED;
 }
 
@@ -303,8 +408,9 @@ static ngx_int_t ngx_http_service_pool_limit_init_shm_zone(ngx_shm_zone_t *shm_z
 
   ctx = shm_zone->data;
 
-  if (octx) {
+  if (octx != NULL) {
     ctx->rbtree = octx->rbtree;
+    ctx->conn = octx->conn;
     return NGX_OK;
   }
 
@@ -312,7 +418,12 @@ static ngx_int_t ngx_http_service_pool_limit_init_shm_zone(ngx_shm_zone_t *shm_z
 
   if (shm_zone->shm.exists){
     ctx->rbtree = shpool->data;
-    return NGX_OK;
+    return NGX_ERROR;
+  }
+
+  ctx->conn = ngx_slab_alloc(shpool, sizeof(ngx_int_t));
+  if (ctx->conn == NULL){
+    return NGX_ERROR;
   }
 
   ctx->rbtree = ngx_slab_alloc(shpool, sizeof(ngx_rbtree_t));
@@ -328,8 +439,9 @@ static ngx_int_t ngx_http_service_pool_limit_init_shm_zone(ngx_shm_zone_t *shm_z
     return NGX_ERROR;
   }
 
-  ctx->conn = 0;
+
   ngx_rbtree_init(ctx->rbtree, sentinel, ngx_http_service_pool_limit_rbtree_insert_value);
+  *ctx->conn = 0;
 
   return NGX_OK;
 }
@@ -351,6 +463,7 @@ static char* ngx_http_service_pool_limit(ngx_conf_t* cf, ngx_command_t* cmd, voi
     return NGX_CONF_ERROR;
   }
 
+
   shm_name = ngx_palloc(cf->pool, sizeof *shm_name);
   shm_name->len = sizeof("service_pool") - 1;
   shm_name->data = (unsigned char *) "service_pool";
@@ -362,6 +475,7 @@ static char* ngx_http_service_pool_limit(ngx_conf_t* cf, ngx_command_t* cmd, voi
 
   shm_zone->init = ngx_http_service_pool_limit_init_shm_zone;
   shm_zone->data = ctx;
+
   splcf->shm_zone = shm_zone;
 
   return NGX_CONF_OK;
